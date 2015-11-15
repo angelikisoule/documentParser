14e0d6cc8f712252fa3b4dc642039224aa76f3bd
angelikisoule <angelikisoule@gmail.com>
2015-11-15 21:24:08 +0200

    implementation person service

--- src/main/java/gr/documentParser/service/implementation/PersonServiceImplementation.java
+++ src/main/java/gr/documentParser/service/implementation/PersonServiceImplementation.java
@@ -1,5 +1,13 @@
 package gr.documentParser.service.implementation;
 
-public class PersonServiceImplementation {
+import gr.documentParser.service.PersonService;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class PersonServiceImplementation implements PersonService{
 
 }
