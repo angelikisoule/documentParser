05c79e004bfdfa6a50ebc291cbc5a2db1e2baebc
blixabargeld <nikolaos.i.papadopoulos@gmail.com>
2015-11-21 10:15:35 +0200

    Fetch Ordering

--- src/main/java/gr/documentParser/model/Interview.java
+++ src/main/java/gr/documentParser/model/Interview.java
@@ -12,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -51,6 +52,7 @@ public class Interview {
 	@Column(name = "filename")
 	private String filename;
 	
+	@OrderBy("id")
 	@Fetch(FetchMode.SELECT)
 	@OneToMany(mappedBy = "interview", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private List<Answer> answers = new ArrayList<Answer>();
